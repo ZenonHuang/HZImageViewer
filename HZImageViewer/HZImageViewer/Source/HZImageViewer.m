04ab00c0ab20f83c9b7ace9b4d7c9e84ca048623
zenon <zzgocc@gmail.com>
2018-01-05 11:32:21 +0800

    update

--- HZImageViewer/HZImageViewer/Source/HZImageViewer.m
+++ HZImageViewer/HZImageViewer/Source/HZImageViewer.m
@@ -4,37 +4,45 @@
 #import "HZImageViewerCell.h"
 
 static NSString *const HZImageViewerCellIdentifier = @"HZImageViewerCellIdentifier";
-
+//HZImageViewerCellDelegate
 @interface HZImageViewer ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
-@property (nonatomic,strong)  UICollectionView *listView;
+@property (nonatomic,strong)  UICollectionView           *listView;
 @property (nonatomic,strong)  UICollectionViewFlowLayout *flowLayout;
 
-@property (nonatomic,strong)  HZImageViewerCell *selectedCell;
-@property (nonatomic,strong)  UITapGestureRecognizer *singleTapGesture;
-@property (nonatomic,assign)  NSInteger pageIndex;
-@property (nonatomic,assign)  CGFloat originPanImageViewCenterX;
-@property (nonatomic,assign)  CGFloat panImageViewCenterX;
+@property (nonatomic,strong)  HZImageViewerCell       *selectedCell;
+@property (nonatomic,strong)  UITapGestureRecognizer  *singleTapGesture;
+@property (nonatomic,assign)  NSInteger                pageIndex;
+
+@property (nonatomic,strong)  UIPageControl          *pageControl;
+@property (nonatomic,assign)  CGFloat                 originPanImageViewCenterX;
+@property (nonatomic,assign)  CGFloat                 panImageViewCenterX;
 @end
 
 @implementation HZImageViewer
+
 #pragma mark - life cycle
 - (void)viewDidLoad{
     [super viewDidLoad];
     
-    self.view .backgroundColor = [UIColor orangeColor];
-    self.singleTapGesture = [[UITapGestureRecognizer alloc] init];
-    self.singleTapGesture.delaysTouchesEnded=YES;
+    self.view .backgroundColor    = [UIColor darkTextColor];
+    self.singleTapGesture         = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                            action:@selector(handleTapGesture:)];
+    self.singleTapGesture.delaysTouchesEnded   = YES;
     self.singleTapGesture.cancelsTouchesInView = YES;
+    [self.view addGestureRecognizer:self.singleTapGesture];
     
-//    UIPanGestureRecognizer *panGesture=[[UIPanGestureRecognizer alloc] initWithTarget:self
-//                                                                               action:@selector(handlePanGesture:)];
-//    panGesture.delaysTouchesEnded=YES;
-//    panGesture.cancelsTouchesInView = YES;
-//    [self.view addGestureRecognizer:panGesture];
+    //    UIPanGestureRecognizer *panGesture = [[ UIPanGestureRecognizer alloc] initWithTarget:self
+    //                                                                                  action:@selector(handlePanGesture:)];
+    //    panGesture.delaysTouchesEnded   = YES;
+    //    panGesture.cancelsTouchesInView = YES;
+    //    [self.view addGestureRecognizer:panGesture];
     
     [self.view addSubview:self.listView];
+    if (self.dataList.count>1) {
+        [self.view addSubview:self.pageControl];
+        self.pageControl.currentPage = self.pageIndex;
+    }
     
-    [self.listView reloadData];
 }
 
 - (void)viewWillLayoutSubviews{
@@ -44,93 +52,104 @@ static NSString *const HZImageViewerCellIdentifier = @"HZImageViewerCellIdentifi
 
 - (void)viewDidLayoutSubviews{
     [super viewDidLayoutSubviews];
-    [self firstSetImageDetail];
+    [self setPageIndexOffSet];
 }
 
+- (BOOL)prefersStatusBarHidden{
+    return YES;
+}
 
+- (void)dealloc{
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 9.0) {
+        return;
+    }
+    // iOS8.x
+    _listView.dataSource = nil;
+    _listView.delegate   = nil;
+}
 #pragma mark - private
 
+- (void)setPageIndexOffSet{
+    CGFloat  screenWidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat  newOffSetX  = screenWidth*(CGFloat)self.pageIndex;
+    CGFloat  totalSpaceX = 20 * (CGFloat)self.pageIndex;
+    CGPoint  newOffSet   = CGPointMake(newOffSetX+totalSpaceX, 0);
+    [self.listView setContentOffset:newOffSet animated:NO];
+}
+
+- (void)handleTapGesture:(UITapGestureRecognizer *)sender {
+    //    [self setNeedsUpdateOfHomeIndicatorAutoHidden];
+    [self dismissViewControllerAnimated:NO completion:nil];
+}
+
 - (void)handlePanGesture:(UIPanGestureRecognizer *)sender{
-    
     switch (sender.state) {
-        case UIGestureRecognizerStateBegan:
-        {
+        case UIGestureRecognizerStateBegan:{
             CGPoint point = [sender locationInView:self.listView];
             NSIndexPath *indexPath = [self.listView indexPathForItemAtPoint:point];
+            self.selectedCell =(HZImageViewerCell *)[self.listView cellForItemAtIndexPath:indexPath];
             
-            UICollectionViewCell *cell = [self.listView cellForItemAtIndexPath:indexPath];
-            self.selectedCell = (HZImageViewerCell *)cell;
             self.originPanImageViewCenterX = self.selectedCell.imageView.center.y;
+            
             [self dismissViewControllerAnimated:YES completion:nil];
             
             break;
         }
-    case UIGestureRecognizerStateChanged:
-        {
-            CGPoint translation = [sender translationInView:self.view];
-            self.panImageViewCenterX = self.selectedCell.imageView.center.y + translation.y;
+        case UIGestureRecognizerStateChanged:{
             
-            CGPoint oldCenter = self.selectedCell.imageView.center;
-            CGPoint newCenter = CGPointMake(oldCenter.x,  self.panImageViewCenterX );
-            self.selectedCell.imageView.center = newCenter;
-
+            CGPoint  translation = [sender translationInView:self.view];
+            self.panImageViewCenterX = self.selectedCell.imageView.frame.origin.y + translation.y;
+            
+            CGPoint oldCenter =  self.selectedCell.imageView.center;
+            self.selectedCell.imageView.center = CGPointMake(oldCenter.x, self.panImageViewCenterX);
+            
             [sender setTranslation:CGPointZero inView:self.view];
-
+            
+            CGFloat vertivalMovement = self.originPanImageViewCenterX - self.panImageViewCenterX;
+            float verticalPercent = fabs(vertivalMovement / self.view.frame.size.height);
+            self.view.alpha = 1 - verticalPercent;
             
             break;
         }
         case UIGestureRecognizerStateCancelled:
         case UIGestureRecognizerStateEnded:
-        case UIGestureRecognizerStateFailed:
-        {
-
-            double velocityY  = fabs([sender velocityInView:self.view].y);
-            BOOL isScrollUp = (self.originPanImageViewCenterX - self.panImageViewCenterX) > 0;
-
+        case UIGestureRecognizerStateFailed:{
+            float velocityY = fabs([sender velocityInView:self.view].y);
+            BOOL  isScrollUp = (self.originPanImageViewCenterX - self.panImageViewCenterX) > 0;
+            
             if (velocityY > 800) {
                 self.view.userInteractionEnabled = NO;
-
+                
                 [UIView animateWithDuration:0.3 animations:^{
+                    self.view.alpha = 0;
                     CGFloat height = self.view.frame.size.height;
-                    CGFloat y = isScrollUp ? -height : height;
-                    
-                    CGSize size = self.selectedCell.frame.size;
-                    CGPoint origin =  self.selectedCell.frame.origin;
+                    CGFloat y      = isScrollUp ? -height : height;
+                    CGPoint oldOrigin = self.selectedCell.frame.origin;
+                    CGSize  oldSize   = self.selectedCell.frame.size;
                     self.selectedCell.frame = (CGRect){
-                        .size   = size,
-                        .origin = CGPointMake(origin.x, y),
+                        .size = oldSize,
+                        .origin =CGPointMake(oldOrigin.x,y)
                     };
                 }];
-
             } else {
-
+                
                 [UIView animateWithDuration:0.3 animations:^{
+                    self.view.alpha = 1;
                     
-                   CGPoint oldCenter = self.selectedCell.imageView.center;
-                   CGPoint newCenter = CGPointMake(oldCenter.x, self.originPanImageViewCenterX);
-                  self.selectedCell.imageView.center = newCenter;
-
+                    CGPoint oldCenter =  self.selectedCell.imageView.center;
+                    self.selectedCell.imageView.center = CGPointMake(oldCenter.x,self.originPanImageViewCenterX);
                 }];
+                
             }
             break;
         }
         default:
             break;
     }
+    
+    
 }
 
-- (void)firstSetImageDetail{
-    [self setPageIndexOffSet];
-
-}
-
-- (void)setPageIndexOffSet{
-    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
-    CGFloat  newOffSetX = screenWidth*(CGFloat)self.pageIndex;
-    CGFloat  totalSpaceX = 20 * (CGFloat)self.pageIndex;
-    CGPoint newOffSet = CGPointMake(newOffSetX+totalSpaceX, 0);
-    [self.listView setContentOffset:newOffSet animated:NO];
-}
 #pragma mark - UIScrollViewDelegate
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
     CGFloat contentOffSetX = scrollView.contentOffset.x;
@@ -138,13 +157,10 @@ static NSString *const HZImageViewerCellIdentifier = @"HZImageViewerCellIdentifi
     NSInteger newPageIndex = (int)round(contentOffSetX / scrollViewWidth);
     
     if (self.pageIndex != newPageIndex ){
-//        setImageDetailText(newPageIndex)
         self.pageIndex = newPageIndex;
+        self.pageControl.currentPage = newPageIndex;
     }
     
-    
-//    CGFloat x =  newPageIndex==0? 0 : newPageIndex*(scrollViewWidth+20);
-//    [scrollView setContentOffset:CGPointMake(x, scrollView.contentOffset.y) animated:YES];
 }
 
 
@@ -156,10 +172,8 @@ static NSString *const HZImageViewerCellIdentifier = @"HZImageViewerCellIdentifi
     
     HZImageViewerCell *displayCell =(HZImageViewerCell *) cell;
     [displayCell.scrollView setZoomScale:1 animated:NO];
-
     
     [self.singleTapGesture requireGestureRecognizerToFail:displayCell.doubleTapGesture];
-    
 }
 #pragma mark - UICollectionViewDataSource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
@@ -168,16 +182,25 @@ static NSString *const HZImageViewerCellIdentifier = @"HZImageViewerCellIdentifi
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-   HZImageViewerCell *cell = [self.listView dequeueReusableCellWithReuseIdentifier:HZImageViewerCellIdentifier
-                                                                           forIndexPath:indexPath];
+    HZImageViewerCell *cell = [self.listView dequeueReusableCellWithReuseIdentifier:HZImageViewerCellIdentifier
+                                                                       forIndexPath:indexPath];
     [cell resetImageView];
     [cell configureImage:self.dataList[indexPath.row]];
+    
+//    cell.delegate = self;
+    
     return cell;
 }
-#pragma mark -
+#pragma mark - UICollectionViewLayout
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
     return     [UIScreen mainScreen].bounds.size;
 }
+
+#pragma mark - WFImageListViewerCellDelegate
+- (void)singleTapImageView{
+    //    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 #pragma mark - getter
 - (UICollectionView *)listView{
     if (!_listView) {
@@ -193,12 +216,14 @@ static NSString *const HZImageViewerCellIdentifier = @"HZImageViewerCellIdentifi
         
         _listView.pagingEnabled = YES;
         _listView.backgroundColor = [UIColor clearColor];
-
+        
         _listView.showsHorizontalScrollIndicator = NO;
         _listView.showsVerticalScrollIndicator   = NO;
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
         if (@available(iOS 11.0, *)) {
             _listView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
         }
+#endif
     }
     return _listView;
 }
@@ -214,4 +239,19 @@ static NSString *const HZImageViewerCellIdentifier = @"HZImageViewerCellIdentifi
     }
     return _flowLayout;
 }
+
+- (UIPageControl *)pageControl{
+    if (!_pageControl) {
+        CGSize screenSize = [UIScreen mainScreen].bounds.size;
+        _pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake( 0, screenSize.height - 30,  screenSize.width, 10)];
+        _pageControl.pageIndicatorTintColor = [UIColor darkGrayColor];
+        _pageControl.currentPageIndicatorTintColor = [UIColor whiteColor];
+        _pageControl.currentPage   = self.pageIndex;
+        _pageControl.numberOfPages = self.dataList.count;
+    }
+    
+    return _pageControl;
+    
+}
+
 @end
